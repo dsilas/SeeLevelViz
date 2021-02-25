e057bc4b02d639b96a4702cbb3e3fa5ffb3a56ca
Simon Bursten <simon@breezometer.com>
2021-02-25 12:37:47 +0200

    more manual imports

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
-import os
 from cx_Freeze import setup, Executable
 import cx_Freeze.hooks
 def hack(finder, module):
     return
 cx_Freeze.hooks.load_matplotlib = hack
-import PyQt5.Qt
 
-os.environ['ETS_TOOLKIT'] = 'qt5'
-os.environ['QT_API'] = 'pyqt5'
-pyqt5_path = os.path.dirname(PyQt5.Qt.__file__)
-
-build_exe_options = {"packages": ["sys", "os", "glob",'subprocess',"pyface.ui.qt4", "tvtk.vtk_module", "tvtk.pyface.ui.wx",'pygments.lexers',
-                                  'tvtk.pyface.ui.qt4','pyface.qt','pyface.qt.QtGui','pyface.qt.QtCore',
-                                  'osgeo','numpy','math','mayavi',"statistics","traitsui","pyface"],
-                     "include_files": [(str(pyqt5_path), "PyQt5.Qt")],
-                     "includes":['mayavi','PyQt5'],
+build_exe_options = {"packages": ["pyface.ui.qt4", "tvtk.vtk_module", "tvtk.pyface.ui.wx", 'pkg_resources._vendor','pkg_resources.extern','pygments.lexers',
+                                  'tvtk.pyface.ui.qt4','pyface.qt','pyface.qt.QtGui','pyface.qt.QtCore','numpy','mayavi'],
+                     "includes":['PyQt5.QtCore','PyQt5.QtGui','mayavi','PyQt5'],
                      'excludes':'Tkinter',
                     "namespace_packages": ['mayavi']
                     }
+
 
 executables = [
     Executable('src/__main__.py', targetName="test",)
 ]
 
-setup(name='Test',
+setup(name='main',
       version='1.0',
       description='',
       options = {"build_exe": build_exe_options},
